e000e3f5d1071b5332cc0b3186ffbde2a1c49bd9
Gaurav Kumar <gauravkumar37@gmail.com>
2015-11-01 22:38:03 +0530

    Optimized TwitterAnalyzer Bumped dependencies Added scaladoc

--- socialzoo-spark-batch/src/main/scala/com/socialzoo/twitter/TwitterAnalyzer.scala
+++ socialzoo-spark-batch/src/main/scala/com/socialzoo/twitter/TwitterAnalyzer.scala
@@ -26,50 +26,59 @@ object TwitterAnalyzer {
 		val keywords: Seq[String] = Seq("messi", "chelsea")
 		//		val keywords: Seq[String] = Seq("messi", "chelsea", "newcastle", "fifa", "football", "engvswal")
 		// (keyword, tweet)
-		val rddPairKeywords: RDD[(String, Row)] = df.flatMap(mapTweetsToKeywords(_, keywords))
+		val rddPairKeywords: RDD[(String, Row)] = df.flatMap(mapTweetToKeywords(_, keywords))
 
 		// Hourly tweets count aggregations
-		// ((keyword, hour), count)
-		val rddPairHourlyCounts: RDD[((String, String), Int)] = rddPairKeywords.map {
+		// ((keyword, date, hour), count)
+		val rddPairHourlyCounts: RDD[((String, String, String), Int)] = rddPairKeywords.map {
 			case (keyword, row) =>
-				((keyword, UtilTwitter.formatDateTime(row.getAs[Long]("created_at"), "yyyy-MM-dd HH")), 1)
+				((keyword, UtilTwitter.formatDateTime(row.getAs[Long]("created_at"), "yyyy-MM-dd"),
+						UtilTwitter.formatDateTime(row.getAs[Long]("created_at"), "HH")), 1)
 		}.reduceByKey(_ + _)
 		rddPairHourlyCounts.foreach(println)
 
 		// Daily tweets count aggregations
-		// ((keyword, day), count)
+		// ((keyword, date), count)
 		val rddPairDateHourlyCounts: RDD[((String, String), Int)] = rddPairHourlyCounts.map {
-			case ((keyword, hour), count) =>
-				((keyword, UtilTwitter.reformatDateTime(hour, "yyyy-MM-dd HH", "yyyy-MM-dd")), count)
+			case ((keyword, date, hour), count) =>
+				((keyword, date), count)
 		}.reduceByKey(_ + _)
 		rddPairDateHourlyCounts.foreach(println)
 
-		// ((keyword, hour, word), count)
-		val rddPairHourlyWordFreq: RDD[((String, String, String), Int)] = rddPairKeywords.map {
+		// ((keyword, date, hour, word), count)
+		val rddPairHourlyWordFreq: RDD[((String, String, String, String), Int)] = rddPairKeywords.map {
 			case (keyword, row) =>
-				((keyword, UtilTwitter.formatDateTime(row.getAs[Long]("created_at"), "yyyy-MM-dd HH")), row.getAs[String]("text"))
+				((keyword, UtilTwitter.formatDateTime(row.getAs[Long]("created_at"), "yyyy-MM-dd"),
+						UtilTwitter.formatDateTime(row.getAs[Long]("created_at"), "HH")), row.getAs[String]("text"))
 		}.flatMapValues {
 			UtilTwitter.tokenizeText
 		}.map {
-			case ((keyword, hour), word) =>
-				((keyword, hour, word), 1)
+			case ((keyword, date, hour), word) =>
+				((keyword, date, hour, word), 1)
 		}.reduceByKey(_ + _)
-		//		rddPairHourlyWordFreq.foreach(println)
+		rddPairHourlyWordFreq.foreach(println)
 
 		val rddPairDailyWordFreq: RDD[((String, String, String), Int)] = rddPairHourlyWordFreq.map {
-			case ((keyword, hour, word), count) =>
-				((keyword, UtilTwitter.reformatDateTime(hour, "yyyy-MM-dd HH", "yyyy-MM-dd"), word), count)
+			case ((keyword, date, hour, word), count) =>
+				((keyword, date, word), count)
 		}.reduceByKey(_ + _)
 		rddPairDailyWordFreq.foreach(println)
 	}
 
-	def mapTweetsToKeywords(row: Row, keywords: Seq[String]): Seq[(String, Row)] = {
+	/**
+	 * Takes in a single row consisting of a single Tweet and maps the tweet to one or more keywords
+	 * depending on the logic specified in [[searchTweet]]
+	 * @param row Twitter row
+	 * @param keywords Seq contains keywords to map Tweets to
+	 * @return array containing (keyword, Row) pairs
+	 */
+	def mapTweetToKeywords(row: Row, keywords: Seq[String]): Seq[(String, Row)] = {
 		var ret: ArrayBuffer[(String, Row)] = ArrayBuffer.empty
 		for (keyword <- keywords) {
 			try {
 				if (searchTweet(row, keyword) ||
-					searchTweet(row.getAs[Row]("retweeted_status"), keyword) ||
-					searchTweet(row.getAs[Row]("quoted_status"), keyword)) {
+						searchTweet(row.getAs[Row]("retweeted_status"), keyword) ||
+						searchTweet(row.getAs[Row]("quoted_status"), keyword)) {
 					ret += ((keyword, row))
 				}
 			} catch {
@@ -80,11 +89,17 @@ object TwitterAnalyzer {
 		ret
 	}
 
+	/**
+	 * searches tweet for the specified keyword
+	 * @param row
+	 * @param keyword
+	 * @return returns true if the tweet's fields contain the specified keyword 
+	 */
 	def searchTweet(row: Row, keyword: String): Boolean = {
 		if (row == null)
 			return false
 		row.getAs[String]("text").toLowerCase.contains(keyword) ||
-			row.getAs[Row]("user").getAs[String]("name").equalsIgnoreCase(keyword) ||
-			row.getAs[Row]("user").getAs[String]("screen_name").equalsIgnoreCase(keyword)
+				row.getAs[Row]("user").getAs[String]("name").equalsIgnoreCase(keyword) ||
+				row.getAs[Row]("user").getAs[String]("screen_name").equalsIgnoreCase(keyword)
 	}
 }
